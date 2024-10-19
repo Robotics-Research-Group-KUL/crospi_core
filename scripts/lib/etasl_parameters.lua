63e4e74375e2768bb2dbf70c9a82a3dfe1799ab7
Erwin Aertbelien <Erwin.Aertbelien@kuleuven.be>
2024-10-19 15:36:07 +0000

    adapted handling of parameters

--- scripts/lib/etasl_parameters.lua
+++ scripts/lib/etasl_parameters.lua
@@ -3,8 +3,16 @@ require("geometric")
 
 -- inspect = require 'inspect'
 
+--- table to record schema info on parameter 
 local vars_info_tab = {}
+
+--- table to record variable names
 local var_names_tab = {}
+
+--- table to record wheter a parameter is optional
+local var_optional_tab = {}
+
+--- description of the task to be embeded in schema
 local task_description = ""
 
 -- maxvel = ctx:createInputChannelScalar("maxvel",0.5)
@@ -13,7 +21,17 @@ function set_task_description(task_descript)
     task_description = task_descript
 end
 
-function createScalarParameter(var_name, default_val, description_val)
+
+
+---create a scalar parameter.
+---
+---@param var_name string parameter name
+---@param default_val number default value of the parameter (not used for optional parameters)
+---@param description_val string description of the parameter to be used in schema
+---@param optional boolean whether or not the parameter is optional
+---@return number|nil number if parameter is optional and not defined, nil is returned
+---
+function createScalarParameter(var_name, default_val, description_val,optional)
     -- local var_tab = {
     --         name=varname,
     --         default=default_val
@@ -23,11 +41,14 @@ function createScalarParameter(var_name, default_val, description_val)
         description_val="The description was not defined. Please provide it when defining the input in the task specification lua file with the createScalarParameter function."
     end
 
+    if optional==nil then
+        optional=false
+    else
+        optional=true
+    end
+    var_optional_tab[var_name] = optional
     table.insert(var_names_tab, var_name)
-
-
     local description_suffix = ". Set as 'external' if the value is not yet known and thus will be set externally at runtime (only once) depending on e.g. the outcome of a previous action or the outcome of another module."
-
     if default_val then
         vars_info_tab[var_name] = {
             description = description_val .. description_suffix,
@@ -40,19 +61,86 @@ function createScalarParameter(var_name, default_val, description_val)
             oneOf= {{ type= "number" }, { enum= {"external"}}}
           }
     end
-
     local exists = rawget(_G, var_name) ~= nil
     if exists then
         print(var_name .. " value: " .. rawget(_G, var_name))
-        return constant(rawget(_G, var_name))
+        return rawget(_G, var_name)
     else
-        print("Warning: " .. var_name .. " does not exist and thus the default value is used. If this code was executed during generation of JSON Schemas, please ignore this warning.")
-        return constant(default_val)
+        if not optional then
+            print("Warning: " .. var_name .. " does not exist and thus the default value is used. If this code was executed during generation of JSON Schemas, please ignore this warning.")
+            return default_val
+        else
+            return nil
+        end
     end
-
-    
     -- return ctx:createInputChannelScalar(var_name, default_val)
 end
+
+
+---
+---Create  a string parameter representing one of a list of string values
+---@param var_name string variable name
+---@param values table table specifying the allowable values
+---@param default_val string  default value
+---@param description_val string description to be used in schema
+---@param optional boolean whether or not the parameter is optional
+---@return string|nil value the value of the parameter, if the parameter is optional and not specified, nil is returned
+---
+function createEnumeratedParameter(var_name, values, default_val, description_val,optional)
+    if not description_val then
+        description_val="The description was not defined. Please provide it when defining the input in the task specification lua file with the createScalarParameter function."
+    end
+
+    if optional==nil then
+        optional=false
+    else
+        optional=true
+    end
+    var_optional_tab[var_name] = optional
+    table.insert(var_names_tab, var_name)
+    if default_val then
+        vars_info_tab[var_name] = {
+            description = description_val,
+            default=default_val,
+            oneOf= { { enum= values }}
+          }
+    else
+        vars_info_tab[var_name] = {
+            description = description_val,
+            oneOf= {{ enum= values }}
+          }
+    end
+    local exists = rawget(_G, var_name) ~= nil
+    if exists then
+        print(var_name .. " value: " .. rawget(_G, var_name))
+        return rawget(_G, var_name)
+    else
+        if not optional then
+            print("Warning: " .. var_name .. " does not exist and thus the default value is used. If this code was executed during generation of JSON Schemas, please ignore this warning.")
+        end
+        return default_val
+    end
+end
+
+
+--- adapt_to_units
+---   adapts the units in the table 
+---@param tbl  table table of numbers
+---@param unit string indicating units ("degrees","radians")
+---@return table adapted tbl
+function adapt_to_units(tbl, unit)
+    if unit=="degrees" then
+        for k,v in pairs(tbl) do
+            tbl[k] = tbl[k] * math.pi/180.0
+        end
+    elseif unit=='radians' then
+        return tbl
+    else
+        error("unknown units")
+    end
+    return tbl
+end
+
 
 -- Later in your Lua script or in an appropriate place, write the vars_info_tab to a file
 function write_json_schema(lua_filepath)
@@ -109,8 +197,10 @@ function write_json_schema(lua_filepath)
 
     for _, key_name in pairs(var_names_tab) do
         -- print(inspect.inspect(vars_info_tab))
-        schema.dependencies["is-"..filename_no_ext].properties[key_name] = vars_info_tab[key_name] 
-        table.insert(schema.dependencies["is-"..filename_no_ext].required, key_name)
+        schema.dependencies["is-"..filename_no_ext].properties[key_name] = vars_info_tab[key_name]
+        if  (var_optional_tab[key_name]~=nil) and not var_optional_tab[key_name] then
+            table.insert(schema.dependencies["is-"..filename_no_ext].required, key_name)
+        end
         -- table.insert(schema.properties["dependent-parameters"].items.enum, key_name)
     end
 
