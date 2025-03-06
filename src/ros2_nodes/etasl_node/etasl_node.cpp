6140229656ea0ea4814d3d3edecaf13ee24834d4
Federico Ulloa <federico.ulloarios@kuleuven.be>
2025-03-06 07:32:14 +0000

    UR10e schema added, UR10e plugin driver loaded and tested in real robot

--- src/ros2_nodes/etasl_node/etasl_node.cpp
+++ src/ros2_nodes/etasl_node/etasl_node.cpp
@@ -745,6 +745,8 @@ void etaslNode::construct_node(){
       //   robotdriver = driver_loader->createSharedInstance("etasl::TemplateDriverEtasl");
       // }
       robotdriver = driver_loader->createSharedInstance("etasl::Ur10eDriverEtasl");
+      // robotdriver = driver_loader->createSharedInstance("etasl::TemplateDriverEtasl");
+
       // robotdriver = driver_loader->createSharedInstance("etasl::KukaIiwaRobotDriver");
 
     }
